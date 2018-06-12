271fa3087b7c61867a3fff780c3bf9085c9a6850
guilloum <guillaume@dev4it.fr>
2018-06-12 13:59:43 +0200

    init spring boot

---
+++ src/main/java/eu/stelliant/irsi/IrsiApplication.java
@@ -0,0 +1,12 @@
+package eu.stelliant.irsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrsiApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/stelliant/irsi/IrsiApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.stelliant.irsi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IrsiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
