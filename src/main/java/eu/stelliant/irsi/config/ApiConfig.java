9e1ea441d30e90bda5ed437404757da7b2af5deb
guilloum <guillaume@dev4it.fr>
2018-06-13 02:14:03 +0200

    config

--- src/main/java/eu/stelliant/irsi/config/ApiConfig.java
+++ src/main/java/eu/stelliant/irsi/config/ApiConfig.java
@@ -2,31 +2,37 @@ package eu.stelliant.irsi.config;
 
 import com.darva.irsi.api.client.ApiClient;
 import com.darva.irsi.api.client.DefaultApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties({ApiProperties.class})
 public class ApiConfig {
 
-  @Value("${irsi.api.baseUrl}")
-  String baseUrl;
+  @Autowired
+  ApiProperties properties;
 
-  @Value("${irsi.api.username}")
-  String username;
-
-  @Value("${irsi.api.password}")
-  String password;
+  public ApiProperties getProperties() {
+    return properties;
+  }
 
   @Bean
   public DefaultApi defaultApi() {
     ApiClient apiClient = new ApiClient();
-    apiClient.setBasePath(baseUrl);
-    apiClient.setUsername(username);
-    apiClient.setPassword(password);
+    if (getProperties().getApi().getDebugging()) {
+      apiClient.setDebugging(true);
+    }
+    apiClient.setBasePath(getProperties().getApi().getBaseUrl());
+    apiClient.setUsername(getProperties().getApi().getUsername());
+    apiClient.setPassword(getProperties().getApi().getPassword());
     return new DefaultApi(apiClient);
   }
 
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper().findAndRegisterModules();
+  }
 }
---
+++ src/main/java/eu/stelliant/irsi/config/ApiProperties.java
@@ -0,0 +1,57 @@
+package eu.stelliant.irsi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "irsi")
+public class ApiProperties {
+
+  private Api api;
+
+  public Api getApi() {
+    return api;
+  }
+
+  public void setApi(Api api) {
+    this.api = api;
+  }
+
+  public static class Api {
+
+    private String baseUrl;
+    private String username;
+    private String password;
+    private Boolean debugging;
+
+    public String getBaseUrl() {
+      return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+      this.baseUrl = baseUrl;
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public void setUsername(String username) {
+      this.username = username;
+    }
+
+    public String getPassword() {
+      return password;
+    }
+
+    public void setPassword(String password) {
+      this.password = password;
+    }
+
+    public Boolean getDebugging() {
+      return debugging;
+    }
+
+    public void setDebugging(Boolean debugging) {
+      this.debugging = debugging;
+    }
+  }
+}
